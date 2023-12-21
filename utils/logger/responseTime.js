565fd2944132c468f5af2ed03376080967661b2e
Andy Lee <106207411@g.nccu.edu.tw>
2023-12-21 09:10:59 +0800

    Delete stadiumModel and userModel test files, and update logResponseTime to include client IP

--- utils/logger/responseTime.js
+++ utils/logger/responseTime.js
@@ -4,8 +4,9 @@ const logResponseTime = (req, res, time) => {
   let method = req.method;
   let url = req.url;
   let status = res.statusCode;
+  const clientIp = req.ip || req.connection.remoteAddress;
 
-  const log = { method: method, url: url, status: Number.parseFloat(status), response_time: Number.parseFloat(time).toFixed(2) };
+  const log = { method: method, url: url, status: Number.parseFloat(status), response_time: Number.parseFloat(time).toFixed(2), client_ip: clientIp };
  
   logger.info(`${method} ${url} ${status} - - ${time.toFixed(2)} ms`, log);
 };
